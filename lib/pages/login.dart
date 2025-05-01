7a2c030134726df111cd2e7c50c2438ea516d499
Luis Hernandez <149843869+LuisJ-H@users.noreply.github.com>
2025-05-01 13:37:23 -0400

    Update login.dart

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -132,40 +132,37 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 }
 
-import 'package:flutter/material.dart';
-
-class Login extends StatelessWidget{
-
+class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
+        body: Container(
+      width: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
             colors: [
               Colors.blue,
               Colors.purple,
-            ]
+            ]),
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
           ),
-        ),
-        child: Column(
-          children: <Widget>[
-            SizedBox(height: 80,),
-            Expanded(child: Container(
-              decoration: BoxDecoration(
+          Expanded(
+              child: Container(
+            decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(60),
                   topRight: Radius.circular(60),
-                )
-              ),
-            ))
-          ],
-        ),
-      )
-    );
+                )),
+          ))
+        ],
+      ),
+    ));
   }
 }
