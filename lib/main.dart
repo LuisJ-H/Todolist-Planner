562220eb8c572f38759616525b99cb953c452657
JtheGamer18 <150290900+JtheGamer18@users.noreply.github.com>
2025-05-01 14:51:35 -0400

    Update
    8

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
