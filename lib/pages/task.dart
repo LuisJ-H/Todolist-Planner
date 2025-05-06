6e3aa8bca42218dbbd2048f64bb3d5b9ee43da15
Luis Hernandez <149843869+LuisJ-H@users.noreply.github.com>
2025-05-05 21:37:14 -0400

    Projects done. Doing tasks now

--- lib/pages/task.dart
+++ lib/pages/task.dart
@@ -10,6 +10,8 @@ class Task extends StatefulWidget {
 class _TaskState extends State<Task> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(child: Text("Task")),
+    );
   }
 }
